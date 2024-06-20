a1bb64f450744060c491f2bdbc7f8c8c5deb683d
p4irin <p4irin.github.io@gmail.com>
2024-06-20 19:56:21 +0200

    chore: refactor file and directory variables

--- src/motion2telegram/configure.py
+++ src/motion2telegram/configure.py
@@ -3,18 +3,26 @@ import subprocess
 
 user = os.getenv('USER')
 pwd = os.getenv('PWD')
-module_path = os.path.dirname(os.path.abspath(__file__))
+module_dir = os.path.dirname(__file__)
 
 
-def create_file_from_template(file_path: str, template_path: str) -> None:
-    with open(template_path) as fh:
+def create_file_from_template(file: str, template: str) -> None:
+
+    with open(template) as fh:
+
         template_content = fh.read()
         file_content = template_content.replace('{% user %}', user) # type: ignore
         file_content = file_content.replace('{% pwd %}', pwd) # type: ignore
-        output = subprocess.run(['which', 'motion2telegram'], capture_output=True)
-        file_content = file_content.replace('{% motion2telegram %}', output.stdout.decode().strip())
+        result = subprocess.run(
+            ['which', 'motion2telegram'],
+            capture_output=True
+        )
+        file_content = file_content.replace(
+            '{% motion2telegram %}', result.stdout.decode().strip()
+        )
 
-    with open(file_path, 'w') as fh:
+    with open(file, 'w') as fh:
+
         fh.write(file_content)
 
 
@@ -27,41 +35,42 @@ def init() -> None:
     Disallow access for anyone else.
     """
 
-    motion_env_path = f'{pwd}/motion.env'
-    subprocess.run(['cp', f'{module_path}/template.motion.env', motion_env_path])
-    subprocess.run(['chmod', 'u+rw-x', motion_env_path])
-    subprocess.run(['chmod', 'g+r-wx', motion_env_path])
-    subprocess.run(['chmod', 'o-rwx', motion_env_path])
+    motion_env = f'{pwd}/motion.env'
+    subprocess.run(['cp', f'{module_dir}/template.motion.env', motion_env])
+    subprocess.run(['chmod', 'u+rw-x', motion_env])
+    subprocess.run(['chmod', 'g+r-wx', motion_env])
+    subprocess.run(['chmod', 'o-rwx', motion_env])
 
 
 
 def configure() -> None:
-    motion_conf_template_path = f'{module_path}/template.motion.conf'
-    motion_conf_path = motion_conf_template_path.replace('template.', '')
-    motion_service_template_path = f'{module_path}/template.motion.service'
-    motion_service_path = motion_service_template_path.replace('template.', '')
-    mobile_phone_scan_service_template_path = f'{module_path}/template.mobile_phone_scan.service'
-    mobile_phone_scan_service_path = mobile_phone_scan_service_template_path.replace('template.', '')
+
+    motion_conf_template = f'{module_dir}/template.motion.conf'
+    motion_conf = motion_conf_template.replace('template.', '')
+    motion_service_template = f'{module_dir}/template.motion.service'
+    motion_service = motion_service_template.replace('template.', '')
+    mobile_phone_scan_service_template = f'{module_dir}/template.mobile_phone_scan.service'
+    mobile_phone_scan_service = mobile_phone_scan_service_template.replace('template.', '')
 
     subprocess.run(['sudo', 'systemctl', 'stop', 'motion.service'])
     subprocess.run(['sudo', 'systemctl', 'stop', 'mobile_phone_scan.service'])
 
-    create_file_from_template(motion_conf_path, motion_conf_template_path)
-    create_file_from_template(motion_service_path, motion_service_template_path)
-    create_file_from_template(mobile_phone_scan_service_path, mobile_phone_scan_service_template_path)
+    create_file_from_template(motion_conf, motion_conf_template)
+    create_file_from_template(motion_service, motion_service_template)
+    create_file_from_template(mobile_phone_scan_service, mobile_phone_scan_service_template)
 
     command = [
-        'cp', motion_conf_path, f'/etc/motion/{os.path.basename(motion_conf_path)}'
+        'cp', motion_conf, f'/etc/motion/{os.path.basename(motion_conf)}'
     ]
     subprocess.run(['sudo', '-S'] + command, check=True)
     command = [
-        'cp', motion_service_path,
-        f'/lib/systemd/system/{os.path.basename(motion_service_path)}'
+        'cp', motion_service,
+        f'/lib/systemd/system/{os.path.basename(motion_service)}'
     ]
     subprocess.run(['sudo', '-S'] + command, check=True)
     command = [
-        'cp', mobile_phone_scan_service_path,
-        f'/lib/systemd/system/{os.path.basename(mobile_phone_scan_service_path)}'
+        'cp', mobile_phone_scan_service,
+        f'/lib/systemd/system/{os.path.basename(mobile_phone_scan_service)}'
     ]
     subprocess.run(['sudo', '-S'] + command, check=True)
     subprocess.run(['sudo', 'systemctl', 'daemon-reload'])
